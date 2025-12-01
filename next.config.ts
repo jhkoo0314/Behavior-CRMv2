1e33de00a9b5684fd3fab3f05fbb17c049706a3b
jhkoo0314 <jhyun06270314@gmail.com>
2025-12-01 17:58:10 +0900

    Remove experimental nodeMiddleware configuration from Next.js settings.

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "img.clerk.com" }],
   },
-  experimental: {
-    // Node.js 런타임을 사용하여 Edge Runtime 호환성 문제 해결
-    nodeMiddleware: true,
-  },
 };
 
 export default nextConfig;
